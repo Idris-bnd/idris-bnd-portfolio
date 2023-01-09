31adc02298e83d7670db99097ae36d7ce142dc13
idris-bnd <idris.pro83@yahoo.com>
2023-01-09 15:44:16 +0100

    ajout des liens de navigations + animations dessus

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setNav } from '../../actions/action';
 import './Navigation.scss';
 
@@ -7,7 +8,7 @@ import './Navigation.scss';
 function Navigation() {
     const dispatch = useDispatch();
     const navOpen = useSelector((state) => state.reducer.webSite.navOpen)
-    
+
     const handleClickNav = () => {
         dispatch(setNav());
     }
@@ -17,6 +18,18 @@ function Navigation() {
        <div onClick={handleClickNav} className={`menu-btn ${navOpen ? 'open' : ''}`}>
             <div className="menu-btn__burger" />
         </div>
+
+        <ul className={`navigationsLinks ${navOpen ? 'open' : ''}`}>
+            <li>
+                <a href="#Home">Home</a>
+            </li>
+            <li>
+                <a href="#Projects">Projects</a>
+            </li>
+            <li>
+                <a href="#Contact">Contact</a>
+            </li>
+        </ul>
     </div>
  )
 }
